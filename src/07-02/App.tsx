948202b21c3cd17b5e2c0d3d1683a311a4e916b5
slintristeza <slintristeza@gmail.com>
2019-06-13 11:03:33 +0900

    feat: 7-3

--- src/07-02/App.tsx
+++ src/07-02/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CharacterList, { Character } from './CharacterList';
+
 import './App.css';
 
 class App extends Component {
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import React, { Component } from 'react';
-import CharacterList, { Character } from './CharacterList';
+import React, { Component, SyntheticEvent } from 'react';
+import { Button, Card, Statistic } from 'semantic-ui-react';
+
 import './App.css';
 
-class App extends Component {
+interface AppState {
+  count: number;
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  // eslint-disable-next-line react/sort-comp
+  increment() {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  }
+
+  increment2 = (e: SyntheticEvent) => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () =>
+    this.setState(prevState => ({
+      count: prevState.count - 1,
+    }));
+
+  decrement2 = (e: SyntheticEvent) => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      count: prevState.count - 1,
+    }));
+  };
+
   render() {
-    const characters: Character[] = [
-      {
-        id: 1,
-        name: '羽咲 綾乃',
-        age: 16,
-        height: 151,
-      },
-      {
-        id: 2,
-        name: '荒垣 なぎさ',
-        age: 18,
-        height: 174,
-      },
-      {
-        id: 3,
-        name: '泉 理子',
-        age: 18,
-      },
-    ];
+    const { count } = this.state;
 
     return (
       <div className="container">
         <header>
-          <h1>はねバド！ キャラクター一覧</h1>
+          <h1>カウンター</h1>
         </header>
-        <CharacterList school="北小町高校" characters={characters} />
+        <Card>
+          <Statistic className="number-board">
+            <Statistic.Label>count</Statistic.Label>
+            <Statistic.Value>{count}</Statistic.Value>
+          </Statistic>
+          <Card.Content>
+            <div className="ui two buttons">
+              <Button color="red" onClick={this.decrement}>
+                -1
+              </Button>
+              <Button color="green" onClick={() => this.increment()}>
+                +1
+              </Button>
+            </div>
+          </Card.Content>
+          <Card.Content>
+            <div className="ui two buttons">
+              <Button color="red" onClick={this.decrement2}>
+                -1
+              </Button>
+              <Button color="green" onClick={this.increment2}>
+                +1
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
       </div>
     );
   }
