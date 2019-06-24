b17b13f2537290c117b34073922bc8cbd345ad71
slintristeza <slintristeza@gmail.com>
2019-06-25 01:16:20 +0900

    feat: 10-3 index,reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './App';
+import counterReducer, { initialState } from './reducer';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import './styles/semantic.min.css';
 
+const store = createStore(counterReducer, initialState);
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root'),
 );
 
