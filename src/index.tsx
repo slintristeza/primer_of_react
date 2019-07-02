5fd97ec5bf6366ea44ec804d03b93fb37da9f320
slintristeza <slintristeza@gmail.com>
2019-07-03 02:37:33 +0900

    fix:11-03 async saga containers,app,index

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
 
 import App from './App';
-import counterReducer, { initialState } from './reducer';
+import reducer from './reducer';
+import rootSaga from './sagas/github';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import './styles/semantic.min.css';
 
-const store = createStore(counterReducer, initialState);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
 
 serviceWorker.unregister();
+sagaMiddleware.run(rootSaga);
